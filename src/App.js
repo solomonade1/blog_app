59cde2ea38af9c0e2c4415b8dbfca896a82bc8a9
Solomon Adegboyega <solomonadegboyega1@gmail.com>
2022-11-02 13:26:17 +0100

    All file added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Topbar from "./components/topbar/Topbar.jsx";
+import Register from "./pages/register/Register.jsx";
+import Login from "./pages/login/Login.jsx";
+import Settings from "./pages/settings/Settings.jsx";
+import Write from "./pages/write/Write.jsx";
+import Home from "./pages/home/Home.jsx";
+import Single from "./pages/single/Single.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context.js";
 function App() {
+  const {user }= useContext(Context)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Topbar />
+      <Routes>
+        <Route exact path="/" element={user ? <Home /> : <Register /> } />
+        <Route exact path="/posts" element={ <Home />} />
+        <Route path="/register" element={user ? <Home/> :<Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+        <Route path="/settings" element={<Settings />} />
+
+      </Routes>
+    </Router>
   );
 }
 
